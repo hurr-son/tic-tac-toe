6aecc9da0627c16dccd7808faa106086ad5cc5ad
hurr-son <hprewett05@gmail.com>
2022-11-10 12:06:36 -0600

    Created a dom caching function and a render function in an IFEE

--- script.js
+++ script.js
@@ -1,29 +1,80 @@
-let gameboard = {
-    init: function() {
-        this.cacheDom()
-        this.render()
 
-        },
-        cacheDom: function() {
-            this.squareEls =
-            function() {
-                    let elsArray = []; 
-                    for(i = 0; i < 9; i++) {
-                    let el = document.createElement('div');
-                    el.classList.add('squareEl');
-                    el.style.width = '3rem'
-                    
-                    elsArray.push(el);
-                }
-                return elsArray;
-            }
-            
-        },
-        render: function() {
-            this.boardWrapperEl = document.getElementsByClassName('gameboard-wrapper')
-            this.squareEls = document.getElementsByClassName('squareEl');
-        }
-        
-    };
+const gameboard = (function() {
     
-    gameboard.init();
+    let elsArray = []; 
+    // create DOM els
+    for(i = 0; i < 9; i++) {
+        let el = document.createElement('div');
+        el.classList.add('squareEl');
+        el.style.width = '5.6rem'
+        el.style.height = '5.6rem'
+        elsArray.push(el);
+        render()
+    }
+    
+    
+    
+    function render() {
+        const boardContainer = document.querySelector(".gameboard")
+        for(const item of elsArray) {
+        boardContainer.appendChild(item)
+    }}
+    
+    
+    
+    
+    
+    
+    
+    
+})()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let gameboard = {
+//     init: function() {
+//         this.cacheDom()
+//         this.render()
+
+//         },
+//         cacheDom: function() {
+//             this.squareEls =
+//             function() {
+//                     let elsArray = []; 
+//                     for(i = 0; i < 9; i++) {
+//                     let el = document.createElement('div');
+//                     el.classList.add('squareEl');
+//                     el.style.width = '3rem'
+                    
+//                     elsArray.push(el);
+//                 }
+//                 return elsArray;
+//             }
+            
+//         },
+//         render: function() {
+//             this.boardWrapperEl = document.getElementsByClassName('gameboard-wrapper')
+//             this.squareEls = document.getElementsByClassName('squareEl');
+//         }
+        
+//     };
+    
+//     gameboard.init();
